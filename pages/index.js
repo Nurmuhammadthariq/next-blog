c1979d35a43a6071822db0f7939d8f2e57f1fa8b
Helpdesk Aplikasi2 (Nur Muhammad Thariq) <Helpdesk.Aplikasi2@kpk.go.id>
2022-10-04 16:22:41 +0700

    feat: create list blog

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,9 @@
 import Head from "next/head";
 import { PostCard, Categories, PostWidget } from "../components";
+import { getPosts } from "../services";
 
-const posts = [
-  {
-    title: "React Testing",
-    excerpt: "Learn React Testing",
-  },
-  {
-    title: "React with Tailwindcss",
-    excerpt: "Learn React with Tailwindcss",
-  },
-];
-
-export default function Home() {
+export default function Home({ posts }) {
+  // console.log(posts)
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -23,7 +14,7 @@ export default function Home() {
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post, index) => (
-            <PostCard post={post} key={post.title} />
+            <PostCard post={post.node} key={index} />
           ))}
         </div>
         <div className="lg:col-span-4 col-span-1">
@@ -35,4 +26,12 @@ export default function Home() {
       </div>
     </div>
   );
+}
+
+// Fetch data
+export async function getStaticProps() {
+  const posts = (await getPosts()) || []
+  return {
+    props: { posts }
+  }
 }
